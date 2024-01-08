d71b3cfa8122e238ed0bf9df7390c18433c5635a
Pranav Virmani <pranav.virmani@accolitedigital.com>
2024-01-08 12:23:58 +0530

    done

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SnakeAndLadderGameComponent } from './snakeandladdergame/snakeandladdergame.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SnakeAndLadderGameComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+      FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
